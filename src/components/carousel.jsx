d546fc7dc98349f0c2eef9c76c04a94bc971df64
rishitaah <161935392+rishitaah@users.noreply.github.com>
2024-05-12 18:38:05 +0530

    Update carousel.jsx

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,46 +1,39 @@
-import React from 'react';
-import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from 'react-icons/bs'
+import React, { useState } from 'react';
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs';
 import './carousel.css';
 
-export const carousel = ({ data }) => {
-    const {slide, setSlide} = useState(0);
+export const Carousel = ({ data }) => {
+  const [slide, setSlide] = useState(0);
 
-    const nextSlide = () => {
-        setSlide(slide === data.length - 1 ? 0 : slide + 1);
-    };
-    const prevSlide = () => {
-        setSlide(slide === 0 ? data.length - 1 :slide - 1);
-    };
+  const nextSlide = () => {
+    setSlide((slide) => (slide === data.length - 1 ? 0 : slide + 1));
+  };
+
+  const prevSlide = () => {
+    setSlide((slide) => (slide === 0 ? data.length - 1 : slide - 1));
+  };
+
   return (
-     <div className="carousel">
-        <BsArrowLeftCircleFill className='arrow arrow-left' onClick={prevSlide}/>
-        {data.map((item, idx) => {
-        return (
-            <img
-             src={item.src} 
-             alt={item.alt} 
-             key={idx} 
-             className= {slide === idx ? "slide": "slideslide-hidden"}
-            />
-        );
-        })}
-        <BsArrowRightCircleFill 
-        className='arrow arrow-right' 
-        onClick={nextSlide} 
+    <div className="carousel">
+      <BsArrowLeftCircleFill className="arrow arrow-left" onClick={prevSlide} />
+      {data.map((item, idx) => (
+        <img
+          src={item.src}
+          alt={item.alt}
+          key={idx}
+          className={slide === idx ? "slide" : "slide slide-hidden"}
         />
-        <span className='indicators'>
-            {data.map((_, idx) => {
-                return(
-                     <button 
-                     key={idx} 
-                     onClick={() => setSlide(idx)} 
-                     className={slide === idx ? "indicator" : "indicator indicator-inactive"}></button>
-                     );
-            })}
-        </span>  </div>
-        );
+      ))}
+      <BsArrowRightCircleFill className="arrow arrow-right" onClick={nextSlide} />
+      <span className="indicators">
+        {data.map((_, idx) => (
+          <button
+            key={idx}
+            onClick={() => setSlide(idx)}
+            className={slide === idx ? "indicator" : "indicator indicator-inactive"}
+          ></button>
+        ))}
+      </span>
+    </div>
+  );
 };
-  
-  
-
-        
