cc6fc5cbc7807fc66275e20bc51534d21aaaa738
rishitaah <161935392+rishitaah@users.noreply.github.com>
2024-05-21 14:04:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,19 @@ import Rnlogo from './components/rnlogo'
 import AboutUs from './components/aboutus'
 import Sponsers from './components/sponsers'
 import Teamrn from './components/Teamrn'
+import HeroSlider from './components/HeroSlider'
+import Carousel from "./components/Carousel";
 
 import './App.css'
 import './index.css'
-
+const images = [
+  "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+];
 
 function App() {
   return (
@@ -17,7 +26,9 @@ function App() {
       <AboutUs />
       <Sponsers />
       <Teamrn />
-     
+      <HeroSlider />
+      <Carousel images={images} />
+        
     </div>
   )
 }
